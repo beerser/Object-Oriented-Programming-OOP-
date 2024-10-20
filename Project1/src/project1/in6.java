4e3d4eeb9d6c503856ac88317cf3230e532e082c
beerser <bearjaiton@gmail.com>
2024-10-20 16:57:16 +0700

    add data lab1

---
+++ Project1/src/project1/in6.java
@@ -0,0 +1,13 @@
+package exercis_1;
+
+
+public class in6 {
+    public static void main(String[] args) {
+        for(int i=1;i<=9;i++){
+            for(int j=1;j<=9;j++){
+                System.out.print(String.format("%5d",j*i));
+            }
+            System.out.println();
+        }
+    }
+}
